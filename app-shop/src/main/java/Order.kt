a6e0db87eb9c93eda315b289967388e92157b126
Nikita Danilov <mirt@mirt.su>
2021-04-29 19:02:18 +0300

    Fix Order mapping
    #10.29

--- app-shop/src/main/java/Order.kt
+++ app-shop/src/main/java/Order.kt
@@ -1,6 +1,8 @@
 package com.webasyst.api.shop
 
+import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
+import com.webasyst.api.adapter.DateTimeAdapter
 import java.util.Calendar
 
 data class Order (
@@ -10,6 +12,7 @@ data class Order (
     val idEncoded: String,
     @SerializedName("total")
     val total: String,
+    @JsonAdapter(DateTimeAdapter::class)
     @SerializedName("create_datetime")
     val createDatetime: Calendar,
     @SerializedName("currency")
